1bb3bd86d8b6762ac7f4a8a463430eab6cee28fa
u19 <u19@ubuNB-valverde19>
2018-10-31 17:09:23 +0100

    Versió 0.4-B del projecte prjava43Valverde19. Branca Master modificada.

--- src/Prjava43Valverde19/Prjava43Valverde19.java
+++ src/Prjava43Valverde19/Prjava43Valverde19.java
@@ -1,7 +1,7 @@
 package Prjava43Valverde19; 
 import java.awt.*;        // using AWT containers and components 
 import java.awt.event.*;  // using AWT events and listener interfaces 
- 
+import java.net.*; 
 // An AWT GUI program inherits the top-level container java.awt.Frame 
 public class Prjava43Valverde19 extends Frame 
       implements ActionListener, WindowListener { 
